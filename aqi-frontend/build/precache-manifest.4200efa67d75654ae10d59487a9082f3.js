99a8c6f912f5c47d5d623df7ec92f8de92d1d452
Teja0075 <93929566+Teja0075@users.noreply.github.com>
2021-12-05 13:44:12 +0530

    Add files via upload

---
+++ aqi-frontend/build/precache-manifest.4200efa67d75654ae10d59487a9082f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c859a8fa9d719db78d718f16db87842d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d00a0f325e21f9adcf2b",
+    "url": "/static/css/main.63b242f5.chunk.css"
+  },
+  {
+    "revision": "0bc50680301f2427bfdb",
+    "url": "/static/js/2.ba5fc5d3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ba5fc5d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d00a0f325e21f9adcf2b",
+    "url": "/static/js/main.b381a422.chunk.js"
+  },
+  {
+    "revision": "a71789f34f22f7ccc29a",
+    "url": "/static/js/runtime-main.af42fbe3.js"
+  }
+]);
